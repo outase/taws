570a0e7b9b49bce1b403dc69741720693a275225
outase <outase.50@gmail.com>
2019-06-04 06:43:10 +0900

    add taws

---
+++ taws/Controllers/tawsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Windows;
+using System.Threading;
+using System.Transactions;
+using System.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.IE;
+using Npgsql;
+using taws.Models;
+using tawsCommons;
+using tawsLibrary;
+
+namespace taws.Controllers
+{
+    public class tawsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TestAction(TestProperty prop, HttpPostedFileWrapper testCaseFile)
+        {
+           //var browerName = ConfigurationManager.AppSettings["BrowserName"];
+            var browerName = prop.testBrowser;
+
+            //テスト実施日時取得
+            string testDateTime = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+
+            //エビデンス用ディレクトリ作成
+            var fileIo = new FileIo();
+            prop.evidenceSavePath = fileIo.CreateEvidencesDirectory(testDateTime, prop);
+            //テストケースファイル保存
+            var resutSaveFile = fileIo.SaveTestCaseFiles(prop, testDateTime, testCaseFile);
+
+            //URL設定(configから)
+            //var prop.testURL = $"{ConfigurationManager.AppSettings["Url"]}member";
+
+            //スクリーンサイズ取得
+            if (prop.testDevice == "pc")
+            {
+                prop.screenWidth = Convert.ToInt32(ConfigurationManager.AppSettings["BrowerWidth"]);
+                prop.screenHeight = Convert.ToInt32(ConfigurationManager.AppSettings["BrowerHeight"]);
+            }
+            else if (prop.testDevice == "sp_iphone")
+            {
+                prop.screenWidth = Convert.ToInt32(ConfigurationManager.AppSettings["BrowerWidthiPhone"]);
+                prop.screenHeight = Convert.ToInt32(ConfigurationManager.AppSettings["BrowerHeightiPhone"]);
+            }
+            else if (prop.testDevice == "sp_android")
+            {
+                prop.screenWidth = Convert.ToInt32(ConfigurationManager.AppSettings["BrowerWidthAndroid"]);
+                prop.screenHeight = Convert.ToInt32(ConfigurationManager.AppSettings["BrowerHeightAndroid"]);
+            }
+
+            //ウィンドウ表示位置設定
+            prop.positionX = 0;
+            prop.positionY = 0;
+
+
+            //テスト実施---------------------------------------
+
+            var excuteTest = new TestExecute();
+
+            //Google Chromeのテスト
+            if (browerName == "chrome")
+            {
+                //Web Driverの設定
+                var option = new ChromeOptions();
+                if (prop.testDevice == "sp_iphone")
+                {
+                    option.AddArgument($"--user-agent=\"{ ConfigurationManager.AppSettings["UserAgentiPhone"] }\"");
+                }
+                else if (prop.testDevice == "sp_android")
+                {
+                    option.AddArgument($"--user-agent=\"{ ConfigurationManager.AppSettings["UserAgentAndroid"] }\"");
+                }
+
+                excuteTest.ExeTest<ChromeDriver>(new ChromeDriver(option), prop);
+            }
+            //FireFoxのテスト
+            else if (browerName == "firefox")
+            {
+                //Web Driverの設定
+                var option = new FirefoxOptions();
+                if (prop.testDevice == "sp_iphone")
+                {
+                    option.SetPreference("general.useragent.override", ConfigurationManager.AppSettings["UserAgentiPhone"]);
+                }
+                else if (prop.testDevice == "sp_android")
+                {
+                    option.SetPreference("general.useragent.override", ConfigurationManager.AppSettings["UserAgentAndroid"]);
+                }
+                //Proxyの設定
+                if (Convert.ToBoolean(ConfigurationManager.AppSettings["SetProxy"]))
+                {
+                    var fireComm = new FireFoxTestCommons();
+                    option = fireComm.SetProxy(option, ConfigurationManager.AppSettings["NonProxyString"]);
+                }
+
+                excuteTest.ExeTest<FirefoxDriver>(new FirefoxDriver(option), prop);
+            }
+            //Edgeのテスト
+            else if (browerName == "edge")
+            {
+                excuteTest.ExeTest<EdgeDriver>(new EdgeDriver(), prop);
+            }
+
+            prop.resultMsg = $@"{ prop.evidenceSavePath } にテスト結果が保存されました。";
+
+            return View("complete", prop);
+        }
+
+        public ActionResult complete()
+        {
+            return View("complete");
+        }
+    }
+}
---
+++ taws/Models/TestProperty.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using tawsCommons.mvc;
+
+namespace taws.Models
+{
+    public class TestProperty : ITestPropertyModelBase
+    {
+        public string testURL { get; set; }
+        public string testCase { get; set; }
+        public string testTerms1 { get; set; }
+        public string testTerms2 { get; set; }
+        public string testTerms3 { get; set; }
+        public string testTerms4 { get; set; }
+        public string testTerms5 { get; set; }
+        public string testBrowser { get; set; }
+        public string testDevice { get; set; }
+        public string screenShotType { get; set; }
+        public string testUserAgent { get; set; }
+        public bool screenCloseFlg { get; set; }
+
+        //エビデンス出力先パス
+        public string evidenceSavePath { get; set; }
+
+        //スクリーンサイズ
+        public int screenWidth { get; set; }
+        public int screenHeight { get; set; }
+
+        //ウィンドウ表示位置
+        public int positionX { get; set; }
+        public int positionY { get; set; }
+
+        //テスト結果
+        public string resultMsg { get; set; }
+        public string resultErrorMsg { get; set; }
+    }
+}
